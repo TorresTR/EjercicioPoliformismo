f9cd28f5998489f64054cd427bbaa896cc87f38b
TorresTR <42515938+TorresTR@users.noreply.github.com>
2019-03-08 09:36:42 -0500

    modificacion, de la clase jet solo deja los metodos obligatorios de la interfaz

--- src/main/java/com/udec/ejerciciopoliformismo/Jet.java
+++ src/main/java/com/udec/ejerciciopoliformismo/Jet.java
@@ -39,24 +39,7 @@ public class Jet extends VehiculoPotenciado{
         this.numeroTurbinas = numeroTurbinas;
     }
 
-    /**
-     * metodo especializado en la clase Jet, que trae de la clase Vehiculos
-     */
-    @Override
-     public void imprimir(){
-        System.out.println("\n-----------Jet------------------");
-        System.out.println("\nSoy el padre Vehiculo imprimiendo desde la clase Jet");
-    }
     
-    
-    /**
-     * metodo especializado en la clase Jet que trae de VehiculoPotenciado
-     */
-    @Override
-    public void impresionVehiculoPotenciado(){
-    
-        System.out.println("\nSoy vehiculoPotenciado y estoy imprimiendo desde la calse Jet");
-    }
     
     /**
      * metodo Encender especializado de la interfaz
